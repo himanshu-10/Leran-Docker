38efa9bf9c68dd9c6a140e1ad62d2c9564f6129c
Ubuntu <ubuntu@ip-172-31-41-193.ap-south-1.compute.internal>
2025-08-31 18:55:29 +0000

    fixed submodules to normal folders

---
+++ javaHelloWorld/simple-java-docker/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date currentDate = new Date();
+        System.out.println("Hello, Himanshu! Current date: " + currentDate);
+	System.out.println("keep working hard ");
+
+    }
+}
